9e240c7e8a15854bde5830d8f8ec86631cbddaf9
louistmt <apenaspara1998@gmail.com>
2023-02-10 12:59:55 +0000

    Removing build folder from repo

--- build/commands/index.mjs
+++
@@ -1,8 +0,0 @@
-import { produceDefinitionList, produceExecsMap } from "../libs/cmds.mjs";
-import archiveCmd from "./cmd-archive.mjs";
-import cancelCmd from "./cmd-cancel.mjs";
-import catCmd from "./cmd-cat.mjs";
-import configCmd from "./cmd-config.mjs";
-import jobsCmd from "./cmd-jobs.mjs";
-export const execsMap = produceExecsMap(archiveCmd, cancelCmd, catCmd, configCmd, jobsCmd);
-export const definitionsList = produceDefinitionList(archiveCmd, cancelCmd, catCmd, configCmd, jobsCmd);
--- build/libs/worker/index.mjs
+++
@@ -1,4 +0,0 @@
-export { default as Worker } from "./Worker.mjs";
-export { default as Task } from "./Task.mjs";
-export { default as Job } from "./Job.mjs";
-export { JobState } from "./types.mjs";

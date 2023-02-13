8169ddca2bca16e6dfc1a604af0e1786fdd9fa8b
louistmt <apenaspara1998@gmail.com>
2023-02-13 16:03:56 +0000

    Fix bug + Added build scripts

---
+++ scripts/build-deploy-cmds.mjs
@@ -0,0 +1,34 @@
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v10";
+
+import {definitionsList} from "../build/commands/index.mjs";
+import Config from "../build/config.mjs";
+
+
+const commands = [];
+
+console.log("Deploying commands");
+
+for (let definition of definitionsList) {
+    commands.push(definition.toJSON());
+}
+
+console.log("Commands");
+console.log(commands);
+
+const rest = new REST({ version: '10' }).setToken(Config.token);
+
+try {
+    console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = await rest.put(
+        Routes.applicationCommands(Config.clientId),
+        { body: commands },
+    );
+
+    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+} catch (error) {
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
+}

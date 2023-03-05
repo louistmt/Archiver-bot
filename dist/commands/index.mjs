d94b70d0bebcff3a133ada6c059e5d0974305a13
[pushdist] <apenaspara1998@gmail.com>
2023-03-05 16:00:09 +0000

    [pushdist]: Pushing new dist files

--- dist/commands/index.mjs
+++ dist/commands/index.mjs
@@ -5,5 +5,6 @@ import catCmd from "./cmd-cat.mjs";
 import configCmd from "./config/index.mjs";
 import jobsCmd from "./cmd-jobs.mjs";
 import exportCmd from "./export/index.mjs";
-export const execsMap = produceExecsMap(archiveCmd, cancelCmd, catCmd, configCmd, jobsCmd, exportCmd);
-export const definitionsList = produceDefinitionList(archiveCmd, cancelCmd, catCmd, configCmd, jobsCmd, exportCmd);
+import exportCatCmd from "./export-category.mjs";
+export const execsMap = produceExecsMap(archiveCmd, cancelCmd, catCmd, configCmd, jobsCmd, exportCmd, exportCatCmd);
+export const definitionsList = produceDefinitionList(archiveCmd, cancelCmd, catCmd, configCmd, jobsCmd, exportCmd, exportCatCmd);

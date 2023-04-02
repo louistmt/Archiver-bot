80a635d432f9aa872a6a3f0f317bc487194ab58a
[pushdist] <apenaspara1998@gmail.com>
2023-04-02 20:37:01 +0000

    [pushdist]: Pushing new dist files

--- dist/commands/jobs.mjs
+++ dist/commands/jobs.mjs
@@ -1,14 +1,14 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { PermissionFlagsBits } from "discord-api-types/v9";
 import Archiver from "../workers/archiver.mjs";
+import { ServersConfigChest } from "../data/index.mjs";
+const serversConfig = ServersConfigChest.get();
 const definition = new SlashCommandBuilder();
 definition.setName("jobs");
 definition.setDescription("Prints info about the archival jobs in queue");
 definition.setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
 definition.setDMPermission(false);
-async function execute(interaction) {
-    const serverId = interaction.guildId;
-    /**
+/**
      * **Archive jobs**\n
      * \n
      * Current job: ${jobName}\n
@@ -22,7 +22,12 @@ async function execute(interaction) {
      *   ... (if more than five are queued)
      *
      */
-    const allJobs = Archiver.jobs.filter((job) => !job.isStateImmutable() && job.data.serverId === serverId);
+async function execute(interaction) {
+    const serverId = interaction.guildId;
+    let { archiveServerId } = serversConfig.getOrCreate(serverId);
+    if (archiveServerId.length === 0)
+        archiveServerId = serverId;
+    const allJobs = Archiver.jobs.filter((job) => !job.isStateImmutable() && job.data.serverId === archiveServerId);
     const firstJobs = allJobs.slice(0, 5);
     if (allJobs.length == 0) {
         await interaction.reply("No active archive jobs at the moment.");
@@ -37,7 +42,7 @@ async function execute(interaction) {
         const { msgs, msgCount } = currentJob.data;
         response.push(`Messages Sent: ${msgCount - msgs.length}/${msgCount}\n`);
     }
-    response.push("**Archive Queue**");
+    response.push(`**Archive Queue (${allJobs.length})**`);
     for (let { data } of firstJobs) {
         const { srcChannelName, destCategoryName } = data;
         response.push(`- \`\`${srcChannelName}\`\` into \`\`${destCategoryName}\`\``);

f75f3e6ea23d0f4680be050b850cc53bd60262b3
louistmt <apenaspara1998@gmail.com>
2023-04-17 14:39:05 +0100

    Finished setting up replit

--- scripts/replit-start.mjs
+++ scripts/replit-start.mjs
@@ -1,8 +1,8 @@
 // Let's try to capture calls into console.log and console.error
 const log = []
 const logMaxSize = 100
-const originalOutWrite = process.stdout.write
-const originalErrWrite = process.stderr.write
+const originalOutWrite = process.stdout.write.bind(process.stdout)
+const originalErrWrite = process.stderr.write.bind(process.stderr)
 process.stdout.write = (data, cb) => {
     if (log.length > logMaxSize) log.shift()
     log.push(data)
@@ -14,28 +14,38 @@ process.stderr.write = (data, cb) => {
     originalOutWrite(data, cb)
 }
 
-import { request } from "https"
+import * as https from "https"
 import express from "express"
 import startup from "../dist/index.mjs"
 
-const PORT = 9091
+const PORT = 10101
 const app = express()
 const scriptStartTime = Date.now()
 
 app.get("/", (req, res) => {
-    const hours = Math.floor(scriptStartTime / (1000 * 60 * 60))
-    const remainderMinutes = scriptStartTime % (1000 * 60 * 60)
+    const timePassed = Date.now() - scriptStartTime
+    const hours = Math.floor(timePassed / (1000 * 60 * 60))
+    const remainderMinutes = timePassed % (1000 * 60 * 60)
     const minutes = Math.floor(remainderMinutes / (1000 * 60))
     const remainderSeconds = remainderMinutes % (1000 * 60)
-    const seconds = Math.floor(scriptStartTime / 1000)
+    const seconds = Math.floor(remainderSeconds / 1000)
 
-    const formatedTime = `${hours >= 10 ? 10 : "0" + hours}:${minutes >= 10 ? 10 : "0" + minutes}:${seconds >= 10 ? 10 : "0" + seconds}`
+    const formatedTime = `${hours >= 10 ? hours : "0" + hours}:${minutes >= 10 ? 10 : "0" + minutes}:${seconds >= 10 ? 10 : "0" + seconds}`
     res.type("text/plain").send(`Bot has been on since ${formatedTime}`)
 })
 app.get("/log", (req, res) => {
     res.type("text/plain").send(log.join(""))
 })
 
+// Ping repl to try to keep it up at least until the next reset
+setInterval(() => {
+    https.get("https://archiver-bot.luisferreira.repl.co", (res) => {
+        console.log(`Pinged self at ${(new Date()).toUTCString()}`)
+        res.resume()
+    })
+}, 1000 * 60 * 15)
+
+// Start up the bot
 await startup()
 
 app.listen(PORT, () => {

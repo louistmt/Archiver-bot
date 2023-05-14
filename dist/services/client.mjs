cd3666541113a80c568da761d7fa6f33252def8e
[pushdist] <apenaspara1998@gmail.com>
2023-05-14 15:40:54 +0000

    [pushdist]: Pushing new dist files

--- dist/services/client.mjs
+++ dist/services/client.mjs
@@ -4,7 +4,10 @@ import { preLogs } from "../utils.mjs";
 const { log, error } = preLogs("Client");
 const client = new Client(Config.clientOptions);
 client.token = Config.token;
-client.on("error", error);
+client.on("error", (err) => {
+    error(err);
+    client.emit("exit");
+});
 client.once("ready", () => {
     log("Bot is online");
     client.user.setActivity("the servant", { type: ActivityType.Playing });

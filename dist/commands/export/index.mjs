f403277c9c00c15c1b3844838fc5ace9aed1fca3
[pushdist] <apenaspara1998@gmail.com>
2023-07-28 16:35:24 +0000

    [pushdist]: Pushing new dist files

--- dist/commands/export/index.mjs
+++ dist/commands/export/index.mjs
@@ -3,12 +3,14 @@ import { PermissionFlagsBits } from "discord-api-types/v10";
 import { produceSubExecsMap } from "../../libs/cmds.mjs";
 import { exportJsonDefinition, exportJsonExecute } from "./json.mjs";
 import { exportWebPageDefinition, exportWebPageExecute } from "./webpage.mjs";
-const subExecsMap = produceSubExecsMap({ definition: exportJsonDefinition, execute: exportJsonExecute }, { definition: exportWebPageDefinition, execute: exportWebPageExecute });
+import { exportTextDefinition, exportTextExecute } from "./text.mjs";
+const subExecsMap = produceSubExecsMap({ definition: exportJsonDefinition, execute: exportJsonExecute }, { definition: exportWebPageDefinition, execute: exportWebPageExecute }, { definition: exportTextDefinition, execute: exportTextExecute });
 const definition = new SlashCommandBuilder();
 definition.setName("export");
 definition.setDescription("Command used to export channels to some format");
 definition.addSubcommand(exportJsonDefinition);
 definition.addSubcommand(exportWebPageDefinition);
+definition.addSubcommand(exportTextDefinition);
 definition.setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
 definition.setDMPermission(false);
 async function execute(interaction) {
---
+++ dist/commands/export/text.mjs
@@ -0,0 +1,20 @@
+import { SlashCommandSubcommandBuilder } from "@discordjs/builders";
+import Exporter from "../../services/exporter.mjs";
+const exportTextDefinition = new SlashCommandSubcommandBuilder();
+exportTextDefinition.setName("text");
+exportTextDefinition.setDescription("Exports a channel to another as a text file");
+exportTextDefinition.addChannelOption(option => option.setName("src-channel")
+    .setDescription("The source of the messages")
+    .setRequired(true));
+exportTextDefinition.addChannelOption(option => option.setName("dest-channel")
+    .setDescription("The destination channel where the file will be posted")
+    .setRequired(true));
+async function exportTextExecute(interaction) {
+    const srcChannel = interaction.options.getChannel("src-channel");
+    const srcChannelId = srcChannel.id;
+    const srcChannelName = srcChannel.name;
+    const destChannelId = interaction.options.getChannel("dest-channel").id;
+    await Exporter.queue(`export-${srcChannelId}`, { format: "text", srcChannelId, srcChannelName, destChannelId });
+    await interaction.reply(`Exporting ${srcChannelName} as a text file`);
+}
+export { exportTextDefinition, exportTextExecute };
